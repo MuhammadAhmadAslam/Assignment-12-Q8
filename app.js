8035fbb10c33ca57fb5adb937c3a18c81bf3d8b6
MuhammadAhmadAslam <prince1ahmed111@gmail.com>
2024-05-04 23:21:08 +0500

    Assignment 12

---
+++ app.js
@@ -0,0 +1,6 @@
+let string = 'JavaScript is the most popular programming language';
+if (string.slice(0,4).toLowerCase() == 'java') {
+    console.log(`This sentence is Starting from Java`);
+}else{
+    console.log(`This sentence is not Starting from Java`);
+}
